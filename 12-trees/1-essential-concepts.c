64628bbe725f1e7247c88966f5b95d01987f44c9
Karina Moreira <kakatinosa@hotmail.com>
2021-03-02 23:09:38 -0300

    Edit file

--- 12-trees/1-essential-concepts.c
+++ 12-trees/1-essential-concepts.c
@@ -7,7 +7,7 @@ not possible to determine by looking, but we can determine the minimum, e.g. "at
 Internal/External Nodes (Non-leaf Nodes/Leaf Nodes or Non-Terminal Nodes/Terminal Nodes):
 External/Terminal/Leaf Nodes: degree 0, Internal/Non-leaf/Non-Terminal Nodes: degree other than 0;
 Levels: counts nodes, starts from 1 (root), level 2 (root's children) and so on;
-Height: counts edges, starts from 0 (root) and so on; Forest: collection of threes (remember that
+Height: counts edges, starts from 0 (root) and so on; Forest: collection of trees (remember that
 a single node can also be considered a tree)
 
 Binary Tree: nodes must have 0, 1 or 2 children.
@@ -29,7 +29,22 @@ Min. height: h = (log base2 (n+1)) - 1
 Max. height: h = n - 1
 In conclusion:
 Height of binary tree: (log base2 (n+1)) - 1 <= h <= n - 1 that is: logarithmic to linear: O(log n) to O(n)
-Number of nodes in a binary tree: h + 1 <= n <= 2^(h+1)-1 that is: linear to exponential: O(n) to O(2^n) */
+Number of nodes in a binary tree: h + 1 <= n <= 2^(h+1)-1 that is: linear to exponential: O(n) to O(2^n)
+
+Internal Nodes vs External Nodes:
+There's a relationship between nodes with degree 2 and degree 0, but not degree 1:
+deg(0) = deg(2) + 1, this is always true for binary trees
+
+Strict Binary Tree:
+Definition: nodes can have either 0 or 2 children, not 1 children.
+Height vs Nodes:
+If height is given:
+For the max. number of nodes is the same for a general binary tree formula: n = 2^(h+1) - 1
+For the min. number of nodes we need a formula: n = 2h + 1
+If nodes are given:
+Max: h = (log base2 (n+1)) - 1
+Min: h = (n - 1)/2
+This means height is ranging from logarithmic to linear, similar to simple binary tree */
 
 int main(void)
 {
