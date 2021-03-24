2bb7871383886094d6dc3cf14131566e6a08ff51
Karina Moreira <kakatinosa@hotmail.com>
2021-03-23 22:04:27 -0300

    Implement circular queue

--- 11-queue/3-circular-queue.c
+++ 11-queue/3-circular-queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* Drawbacks of Queue using Arrays:
 1. Cannot use spaces of deleted elements;
@@ -18,9 +19,86 @@ operation: rear = (rear + 1) % size
 
 */
 
-int main(void)
+struct Queue
 {
-    return 0;
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+void create(struct Queue *q, int size)
+{
+    q->size = size;
+    q->Q = (int *)malloc(q->size * sizeof(int));
+    q->front = q->rear = 0;
 }
 
+bool isFull(struct Queue q)
+{
+    if ((q.rear + 1) % q.size == q.front)
+        return true;
+    return false;
+}
 
+bool isEmpty(struct Queue q)
+{
+    if (q.rear == q.front)
+        return true;
+    return false;
+}
+
+void enqueue(struct Queue *q, int x)
+{
+    if (isFull(*q))
+        printf("Queue is full!\n");
+    else
+    {
+        q->rear = (q->rear + 1) % q->size;
+        q->Q[q->rear] = x;
+    }
+}
+
+int dequeue(struct Queue *q)
+{
+    int x = -1;
+    if (isEmpty(*q))
+        printf("Queue is empty!\n");
+    else
+    {
+        q->front = (q->front + 1) % q->size;
+        x = q->Q[q->front];
+    }
+    return x;
+}
+
+void display(struct Queue q)
+{
+    int i = q.front + 1;
+    do
+    {
+        printf("%d ", q.Q[i]);
+        i = (i+1) % q.size;
+    } while (i != (q.rear + 1) % q.size);
+
+    printf("\n");
+}
+
+int main(void)
+{
+    struct Queue q;
+    create(&q, 5);
+
+    enqueue(&q, 10);
+    enqueue(&q, 20);
+    enqueue(&q, 30);
+    enqueue(&q, 40);
+    enqueue(&q, 50);
+    enqueue(&q, 60);
+
+    display(q);
+
+    printf("%d ",dequeue(&q));
+
+    return 0;
+}
