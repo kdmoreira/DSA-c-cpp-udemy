25de715e72f154a29a933262cd810bc930812c6e
Karina Moreira <kakatinosa@hotmail.com>
2021-04-07 23:33:03 -0300

    Add file

---
+++ 14-AVL-trees/1-insert.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* AVL Trees: highly balanced binary search trees.
+Balance factor: height of left subtree - height of right subtree: bf = hl - hr = {-1, 0, 1}
+bf = | hl - hr | <= 1, if: bf = | hl - hr | > 1, it's unbalanced. It's calculated for every
+node in the tree. Balancing is performed by rotations.
+Rotations:
+
+*/
+
+int main(void)
+{
+    return 0;
+}
